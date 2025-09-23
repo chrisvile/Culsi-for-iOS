91a15fe9c1d00ba3a1439630d929e63846686f86
Chris Villarreal <chris@chrisvile.com>
2025-09-23 18:06:59 -0500

    changed tests path

---
+++ Culsi/CulsiTests2/CulsiTests2.swift
@@ -0,0 +1,16 @@
+//
+//  CulsiTests2.swift
+//  CulsiTests2
+//
+//  Created by Chris Vile on 9/23/25.
+//
+
+import Testing
+
+struct CulsiTests2 {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
